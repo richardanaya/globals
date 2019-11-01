7994a7d668dba1d418a4047baafefd4e8ebf84c3
richard <richard>
2019-11-01 16:36:22 -0700

    locking globals

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,54 @@
+#![no_std]
 #[macro_use]
 extern crate lazy_static;
-use std::any::TypeId;
-use std::sync::Mutex;
-use std::collections::HashMap;
-use std::any::Any;
+extern crate alloc;
+use alloc::boxed::Box;
+use core::any::TypeId;
+use spin::Mutex;
+use core::any::Any;
+use hashbrown::HashMap;
+
 
 lazy_static! {
-    static ref GLOBALS:Mutex<HashMap<TypeId, Box<dyn Any+Send>>> = {
+    static ref GLOBALS:Mutex<HashMap<TypeId, Mutex<Box<dyn Any+Send>>>> = {
         Mutex::new(HashMap::new())
     };
 }
 
-pub fn get<T>() -> &'static T where T:'static+Default+Send{
+pub fn get<T>() -> &'static Mutex<Box<T>> where T:'static+Default+Send{
     {
         let id = TypeId::of::<T>();
-        let mut globals = GLOBALS.lock().unwrap();
+        let mut globals = GLOBALS.lock();
         if let Some(v) = globals.get(&id) {
-            return unsafe { &*(v as *const Box<dyn Any+Send> as *const Box<T>) };
+            let m = unsafe { &*(v as *const Mutex<Box<dyn Any+Send>> as *const Mutex<Box<T>>) };
+            return m;
         }
         let v = T::default();
-        globals.insert(id,Box::new(v));
+        globals.insert(id,Mutex::new(Box::new(v)));
     }
     get::<T>()
+}
+
+
+#[derive(Debug)]
+struct Foo {
+    v:u32
+}
+
+impl Default for Foo{
+    fn default() -> Self {
+        Foo{v:42}
+    }
+}
+
+#[test]
+fn simple(){
+    {
+        let mut f = get::<Foo>().lock();
+        assert_eq!(42,f.v);
+        f.v = 50;
+        assert_eq!(50,f.v);
+    }
+    let f = get::<Foo>().lock();
+    assert_eq!(50,f.v);
 }
