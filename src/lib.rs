08337209f4e75c0a4ee72d9d7754ca0da53734c3
richard <richard>
2019-11-01 16:49:31 -0700

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -3,28 +3,29 @@
 extern crate lazy_static;
 extern crate alloc;
 use alloc::boxed::Box;
-use core::any::TypeId;
-use spin::Mutex;
 use core::any::Any;
+use core::any::TypeId;
 use hashbrown::HashMap;
-
+use spin::Mutex;
 
 lazy_static! {
-    static ref GLOBALS:Mutex<HashMap<TypeId, Mutex<Box<dyn Any+Send>>>> = {
-        Mutex::new(HashMap::new())
-    };
+    static ref GLOBALS: Mutex<HashMap<TypeId, Mutex<Box<dyn Any + Send>>>> =
+        { Mutex::new(HashMap::new()) };
 }
 
-pub fn get<T>() -> &'static Mutex<Box<T>> where T:'static+Default+Send{
+pub fn get<T>() -> &'static Mutex<Box<T>>
+where
+    T: 'static + Default + Send,
+{
     {
         let id = TypeId::of::<T>();
         let mut globals = GLOBALS.lock();
         if let Some(v) = globals.get(&id) {
-            let m = unsafe { &*(v as *const Mutex<Box<dyn Any+Send>> as *const Mutex<Box<T>>) };
+            let m = unsafe { &*(v as *const Mutex<Box<dyn Any + Send>> as *const Mutex<Box<T>>) };
             return m;
         }
         let v = T::default();
-        globals.insert(id,Mutex::new(Box::new(v)));
+        globals.insert(id, Mutex::new(Box::new(v)));
     }
     get::<T>()
 }
