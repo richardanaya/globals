f427f1e4c93215efff5052071b71b61d70e0549c
richard <richard>
2019-11-02 00:57:37 -0700

    no lazy_static

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ impl Default for Foo {
   }
 }
 
-let f: Foo = globals::get().lock();
+let f = globals::get::<Foo>().lock();
 
 assert_eq!(f.v,42);
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,70 @@
 #![no_std]
-#[macro_use]
-extern crate lazy_static;
 extern crate alloc;
 use alloc::boxed::Box;
 use core::any::Any;
 use core::any::TypeId;
-use hashbrown::HashMap;
 use spin::Mutex;
+use alloc::collections::linked_list::LinkedList;
 
-lazy_static! {
-    static ref GLOBALS: Mutex<HashMap<TypeId, Mutex<Box<dyn Any + Send>>>> =
-        { Mutex::new(HashMap::new()) };
-}
+static mut GLOBALS_LIST:Option<Mutex<LinkedList<(TypeId,Mutex<Box<dyn Any + Send>>)>>> = None;
 
 pub fn get<T>() -> &'static Mutex<Box<T>>
 where
     T: 'static + Default + Send,
 {
     {
+        let mut globals = unsafe {
+            if GLOBALS_LIST.is_none() {
+                GLOBALS_LIST = Some(Mutex::new(LinkedList::new()));
+            }
+            GLOBALS_LIST.as_ref().unwrap().lock()
+        };
         let id = TypeId::of::<T>();
-        let mut globals = GLOBALS.lock();
-        if let Some(v) = globals.get(&id) {
-            let m = unsafe { &*(v as *const Mutex<Box<dyn Any + Send>> as *const Mutex<Box<T>>) };
+        let p = globals.iter().find(|&r| r.0 == id);
+        if let Some(v) = p {
+            let m = unsafe { &*(&v.1 as *const Mutex<Box<dyn Any + Send>> as *const Mutex<Box<T>>) };
             return m;
         }
         let v = T::default();
-        globals.insert(id, Mutex::new(Box::new(v)));
+        globals.push_front((id, Mutex::new(Box::new(v))));
     }
     get::<T>()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+    use alloc::vec::Vec;
+
+    #[derive(Default)]
+    struct Foo {x:u32}
+
+    #[derive(Default)]
+    struct Bar {x:u32}
+
+
+    #[test]
+    fn basic() {
+        let f = get::<Foo>().lock();
+        let b = get::<Bar>().lock();
+        assert_eq!(0,b.x);
+        assert_eq!(0,f.x);
+    }
+
+    #[test]
+    fn basic_2() {
+        {
+            let mut f = get::<Vec<u32>>().lock();
+            f.resize(2_000_000, 0);
+        }
+        {
+            let mut f = get::<Vec<u32>>().lock();
+            assert_eq!(2_000_000,f.len());
+            f.resize(100, 0);
+        }
+        {
+            let f = get::<Vec<u32>>().lock();
+            assert_eq!(100,f.len());
+        }
+    }
 }
